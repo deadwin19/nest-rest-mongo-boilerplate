358b88f7c0b585fc7fefb40632e61f53295f4236
Jay <69726384+deadwin19@users.noreply.github.com>
2022-01-26 19:57:48 +0530

    1st local commit

---
+++ src/modules/customer/customer.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerSchema } from './schemas/customer.schema';
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Customer', schema: CustomerSchema }])
+  ],
+  controllers: [CustomerController],
+  providers: [CustomerService]
+})
+export class CustomerModule { }
